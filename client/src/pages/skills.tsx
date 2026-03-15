2c0456830281a5ea3a2754a4b58f91c720eb74f9
Thirumalai-Tech-Developer <thirutechdeveloper@gmail.com>
2026-03-15 23:55:16 +0400

    fix

--- client/src/pages/skills.tsx
+++ client/src/pages/skills.tsx
@@ -71,7 +71,7 @@ export default function Skills() {
           ))}
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
 
           {filteredSkills.map((skill, index) => (
             <motion.div
